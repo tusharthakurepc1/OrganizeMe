732632e5c4bba4459d3fd7690188ea9dd2b886b7
Tushar <tusharthakurepc205@gmail.com>
2023-01-23 23:18:13 +0530

    structure chart & jdbc file v1.0.3

--- Project/src/com/Main.java
+++ Project/src/com/Main.java
@@ -1,8 +1,18 @@
 package com;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Main Updated.");
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/java_projects","root","epc205");
+            System.out.println("Connection Open");
+            con.close();
+        }catch(Exception e){
+            System.out.println("Exception.");
+            System.out.println(e);
+        }
     }
 }
