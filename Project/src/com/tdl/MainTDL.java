37dbb1a84a70e901ff256729437407300774cf1f
Manasvi Kumar Thakur <manasvikumar108@gmail.com>
2023-02-27 18:15:20 +0530

    v1.0.9

--- Project/src/com/tdl/MainTDL.java
+++ Project/src/com/tdl/MainTDL.java
@@ -7,9 +7,13 @@ public class MainTDL {
     public static void main(String[] args) {
         System.out.println("Main TDL");
 
-//        LoginGui gui=new LoginGui();
-//        gui.init_Signup();
-//        gui.init_Login();
+
+
+        LoginGui gui=new LoginGui();
+        gui.homeGUI();
+        gui.init_Signup();
+
+
 
 //        Client c1=new Client("User"," ","001",10,"_","+91 xxxxxxxxxx","user123","123");
 //        Task t1=new Task(c1,"Task1","Wake Up 7.00 AM","Imp");
